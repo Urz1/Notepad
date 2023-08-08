0e0777150340f20d93c4cec3cb51dd9744cd2d97
Urz1 <101430982+Urz1@users.noreply.github.com>
2023-08-08 21:33:27 +0300

    Update RegisterPage.java

--- RegisterPage.java
+++ RegisterPage.java
@@ -1,7 +1,28 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class RegisterPage {
-    JFrame frame=new JFrame();
-    
-    
+public class RegisterPage implements ActionListener {
+    JFrame frame;
+    JButton submit;
+    RegisterPage(){
+        submit=new JButton();
+        submit.addActionListener(this);
+        frame=new JFrame();
+        frame.setSize(700,700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.red);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==submit){
+            WelcomePage welcomePage= new WelcomePage();
+
+        }    
+    }
 }
