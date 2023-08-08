2204f09ee80420fc83dfe5d29179ceeb9e1f9b4a
Urz1 <101430982+Urz1@users.noreply.github.com>
2023-08-08 21:33:05 +0300

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -1,6 +1,15 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class LoginPage {
     JFrame frame= new JFrame();
-    
+    LoginPage(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.black);
+
+
+    }
 }
